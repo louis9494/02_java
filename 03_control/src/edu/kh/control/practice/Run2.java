5dcf1d8401772c2cd6f534f4b2c9a1e2826f325c
louis9494 <louis9494@naver.com>
2024-09-04 17:35:24 +0900

    최종완료

--- 03_control/src/edu/kh/control/practice/Run2.java
+++ 03_control/src/edu/kh/control/practice/Run2.java
@@ -4,9 +4,9 @@ public class Run2 {
 	public static void main(String[] args) {
 	LoopPractice LoopEx = new LoopPractice();
 	//LoopEx.practice1();
-	LoopEx.practice3();
+	//LoopEx.practice5();
 	//LoopEx.practice7();
-	//LoopEx.practice9();
+	LoopEx.practice13();
 	// 
 	
 	}
