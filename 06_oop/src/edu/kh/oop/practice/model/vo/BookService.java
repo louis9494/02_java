2ea9d051c28f16a10ad31446b1b48e60741f3fe2
louis9494 <louis9494@naver.com>
2024-09-05 17:22:52 +0900

    240905최종완료

--- 06_oop/src/edu/kh/oop/practice/model/vo/BookService.java
+++ 06_oop/src/edu/kh/oop/practice/model/vo/BookService.java
@@ -3,12 +3,39 @@ package edu.kh.oop.practice.model.vo;
 public class BookService {
 	
 	public void practice() {
+		Book m1 = new Book();
 		
+		System.out.println(m1.toString() );
+		
+		m1.setTitle("자바의정석");
+		m1.setPrice(30000);
+		m1.setDiscountRate(0.2);
+		m1.setAuthor("남궁성");
+		
+		System.out.println(m1.toString() );
+		
+		System.out.println("=============================");
+		
+		System.out.println("수정된 결과 확인");
+		Book m2 = new Book();
+		m2.setTitle("c언어");
+		m2.setPrice(30000);
+		m2.setDiscountRate(0.1);
+		m2.setAuthor("홍길동");
 	
+		System.out.println(m2.toString() );
+		
+		System.out.println("===========================");
 		
 		
 		
+		int money = 30000;
 		
+		
+		System.out.println("도서명 = " + "c언어");
+		System.out.println("할인된 가격 = "+(int)(money*0.9)+"원");
+		System.out.println("도서명 = " + "자바의정석");
+		System.out.println("할인된 가격 = "+(int)(money*0.8)+"원");
 	}
 
 }
